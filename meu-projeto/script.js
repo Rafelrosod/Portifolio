0fa4c3fde7b13d7ed57687297e84489f9bdd801c
r_souza <Rafaelrosod@outlook.com>
2024-08-13 17:02:12 -0300

    Em desenvolvimento.

--- meu-projeto/script.js
+++
@@ -1,6 +0,0 @@
-function showPage(pageId) {
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.classList.remove('active'));
-    const page = document.getElementById(pageId);
-    page.classList.add('active');
-}
